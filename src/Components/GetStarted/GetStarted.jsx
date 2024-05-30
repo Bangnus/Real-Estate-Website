9ed007b7a447c6af825c1e2847cebc16e08684fd
Bangnus <Peerapatlaelem001@gmail.com>
2024-05-30 16:56:50 +0700

    Getstart Section

---
+++ src/Components/GetStarted/GetStarted.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './GetStarted.css'
+const GetStarted = () => {
+    return (
+        <section className="g-erapper">
+            <div className="paddings innerWidth g-container">
+                <div className="flexColCenter inner-container">
+                    <span className='primaryText'>Get started with Homyz</span>
+                    <span className='secondaryText'>Subscribe and find super attractive price quotes from us. <br />
+                        Find your residence soon
+                    </span>
+                    <button className="button">
+                        <a href="#">Get Start</a>
+                    </button>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default GetStarted
